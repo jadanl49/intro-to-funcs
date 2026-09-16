a2cdb9a2eefd34f73cb874e2f9997dd9eec21851
jadanl49 <jadanl3@nycstudents.net>
2026-09-16 09:20:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,32 @@ t.shape('turtle')
 
 """ t.forward(200)"""
 
-def message(input):
+""" def message(input):
     print(input)
 message("Hello Class")
 
-""" def square(x):
+def add(x,y) :
+        #returning function
+        return x + y  """
+
+
+
+def square(x):
     t.forward(125)
     t.left(90)
-
     t.forward(100)
     t.left(90)
-
     t.forward(125)
-    t.left(90)
- 
+    t.left(90)   
+    t.forward(100)
+square(200)
+""" def square(y):
     t.forward(100)
     t.left(90)
-    square(200) """
+    t.forward(100)
+    t.left(90)
+    forward
+square(100) """
 
 
 
@@ -33,6 +42,6 @@ def equal(x):
     t.forward(x)
     t.left(120)
     t.forward(x)
-equal(90) 
+equal(90)  
 
 turtle.done()
