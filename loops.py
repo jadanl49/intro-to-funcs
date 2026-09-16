8ee12632268a7cfe2a952c7c6bb2f7b81091b6da
jadanl49 <jadanl3@nycstudents.net>
2026-09-16 09:20:40 -0400

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,18 @@
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+
+
+
+for i in range(61):
+    t.forward(100)
+    t.left(90)   
+    t.forward(100)   
+    t.left(90)    
+    t.forward(100)
+    t.left(90)
+    t.forward(100)        
+    t.left(90)
+    t.left(5)
+   
