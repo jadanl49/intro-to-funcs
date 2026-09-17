634fb2370849af05c05b38b9f4df0043308e6251
jadanl49 <jadanl3@nycstudents.net>
2026-09-17 09:19:17 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -5,14 +5,30 @@ t.shape('turtle')
 
 
 
-for i in range(61):
-    t.forward(100)
-    t.left(90)   
-    t.forward(100)   
-    t.left(90)    
-    t.forward(100)
-    t.left(90)
-    t.forward(100)        
-    t.left(90)
-    t.left(5)
-   
+# for i in range(61):
+#     t.speed(0)
+#     t.forward(100)
+#     t.left(90)   
+#     t.forward(100)   
+#     t.left(90)    
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(100)        
+#     t.left(90)
+#     t.left(5
+def square(x,y):
+    for i in range(60):
+     t.speed(0)
+     t.forward(x)
+     t.left(y) 
+square(100,90) 
+
+
+           
+def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length = length * 2
+doubleSquares(5)  
+
