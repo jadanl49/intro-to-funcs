dcb49d843d638d94d33212ac6fd8424dc79b2339
jadanl49 <jadanl3@nycstudents.net>
2026-09-18 09:07:14 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+#string for characters
+name = "ddd"
+#input asks the user a question and record the answers
+#what we write in the inpit argument is what user sees
+bill = int(input("wasd"))
+print(bill)
+
+
+#integer for whole number
+amt = 100
+#float uses decimal 
+amt_two = 99.99
+
+if bill == 10:
+    print("swasd")
+else:
+    print("no match")
+
+#boolean
+x = True
+y = False
